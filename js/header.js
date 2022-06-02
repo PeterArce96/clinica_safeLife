3a506c899d7a6952f3a65c6e3a0d33b56ce295db
PeterArce96 <parce0496@gmail.com>
2022-06-01 22:02:22 -0500

    headerNavThemeIconContainer, cambio de tema

--- js/header.js
+++ js/header.js
@@ -1,12 +1,21 @@
     const header = () => {
+        const headerNavThemeIconContainer = document.getElementById('headerNavThemeIconContainer');
+
         const headerNavMenuIcon = document.getElementById('headerNavMenuIcon');
         const headerNavMenuLinkList = document.querySelector('.header-nav__menu-link-list');
-    
-        const toggleMenu = () => {
+        
+        const openMenu = () => {
         headerNavMenuLinkList.classList.toggle('header-nav__menu-link-list--open');
         };
-    
-        headerNavMenuIcon.addEventListener('click', toggleMenu);
+        const toggleMenu = () => {
+            const body = document.querySelector('.body');
+            body.classList.toggle('body--dark');
+            headerNavThemeIconContainer.classList.toggle('header-nav__theme-icon-container--active');
+        };
+        
+        headerNavThemeIconContainer.addEventListener('click', toggleMenu);
+        headerNavMenuIcon.addEventListener('click', openMenu);
+        
     }
     
     export default header;
