3c18cd45434dec93eeb8bdb484b3c4b25f01bad6
PeterArce96 <parce0496@gmail.com>
2022-06-08 22:52:47 -0500

    header scroll y menú scroll

--- js/header.js
+++ js/header.js
@@ -1,10 +1,18 @@
     const header = () => {
+        const header = document.querySelector('.header');
+
         const headerNavThemeIconContainer = document.getElementById('headerNavThemeIconContainer');
 
         const headerNavMenuIcon = document.getElementById('headerNavMenuIcon');
         const headerNavMenuLinkList = document.querySelector('.header-nav__menu-link-list');
         const headerNavMenuLinkItems = document.querySelectorAll('.header-nav__menu-link-item');
         
+
+        const documentScroll = () => {
+            header.classList.toggle('header--scroll', window.scrollY > 0);
+            headerNavMenuLinkList.classList.toggle('header-nav__menu-link-list--scroll', window.scrollY > 0);
+        };
+
         const openMenu = () => {
         headerNavMenuLinkList.classList.toggle('header-nav__menu-link-list--open');
         };
@@ -17,6 +25,8 @@
             headerNavMenuLinkList.classList.remove('header-nav__menu-link-list--open');
         };
         
+
+        document.addEventListener('scroll', documentScroll);
         headerNavThemeIconContainer.addEventListener('click', toggleMenu);
         headerNavMenuIcon.addEventListener('click', openMenu);
         headerNavMenuLinkItems.forEach((element) =>{
