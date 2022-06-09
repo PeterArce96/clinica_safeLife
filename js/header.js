29816b3d8771b1e61024a2d4a6c5d9802e0ae7e7
PeterArce96 <parce0496@gmail.com>
2022-06-08 23:02:05 -0500

    localstorage theme

--- js/header.js
+++ js/header.js
@@ -13,18 +13,35 @@
             headerNavMenuLinkList.classList.toggle('header-nav__menu-link-list--scroll', window.scrollY > 0);
         };
 
-        const openMenu = () => {
-        headerNavMenuLinkList.classList.toggle('header-nav__menu-link-list--open');
-        };
         const toggleMenu = () => {
             const body = document.querySelector('.body');
             body.classList.toggle('body--dark');
             headerNavThemeIconContainer.classList.toggle('header-nav__theme-icon-container--active');
+            
+            if (body.classList.contains('body--dark')) {
+                localStorage.setItem('lightMode', 'false');
+            } else {
+                localStorage.setItem('lightMode', 'true');
+            }
         };
+
+        const openMenu = () => {
+        headerNavMenuLinkList.classList.toggle('header-nav__menu-link-list--open');
+        };
+        
+
         const closeMenu = () => {
             headerNavMenuLinkList.classList.remove('header-nav__menu-link-list--open');
         };
         
+        if (localStorage.getItem('lightMode') === 'true' || localStorage.getItem('lightMode') === null) {
+            document.querySelector('.body').classList.remove('body--dark');
+            headerNavThemeIconContainer.classList.remove('header-nav__theme-icon-container--active');
+        } else {
+            document.querySelector('.body').classList.add('body--dark');
+            headerNavThemeIconContainer.classList.add('header-nav__theme-icon-container--active');
+        }
+
 
         document.addEventListener('scroll', documentScroll);
         headerNavThemeIconContainer.addEventListener('click', toggleMenu);
